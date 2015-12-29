97ad9d06bec99dfa9f04eb1e627888196f503f40
higa <higa@higa-no-MacBook-Air.local>
2015-12-29 18:07:45 +0900

    nocopochiコントローラーを新規作成

---
+++ app/controllers/notopochi_controller.rb
@@ -0,0 +1,2 @@
+class NotopochiController < ApplicationController
+end
---
+++ test/controllers/notopochi_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NotopochiControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
