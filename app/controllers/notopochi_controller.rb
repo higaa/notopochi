02483ea218f405e43266307600f2462ce43239fc
higa <higa@higa-no-MacBook-Air.local>
2016-01-10 17:08:39 +0900

    コントローラーの修正

--- app/controllers/notopochi_controller.rb
+++ app/controllers/notopochi_controller.rb
@@ -7,10 +7,31 @@ class NotopochiController < ApplicationController
 	end
 	
 	def do_login
-		redirect_to action: :departure
+		user_id = params[:user]
+		password = params[:pass]
+		
+		#entry = DB検索(user_id)
+		
+		#if !found then
+		#	redirect_to action: :login
+		#elsif entry.password != password then
+		#	redirect_to action: :login
+		#else
+			redirect_to action: :departure
+		#end
 	end
 	
 	def departure
+		
+		#departureUrl = DB検索(出発地点のURL)
+		#getAndSave(departureUrl, departureFile)
+		
+		#@departurePoints = ["金沢駅(ダミーデータ)", "香林坊(ダミーデータ)"]
+		@departurePoints = [ { :id => "1", :name => "金沢駅(ダミーデータ)" },
+			{ :id => "2", :name => "香林坊(ダミーデータ)" } ]
+		#CSV.foreach(departureFile) do |row|
+		#	@departurePoints.push(row[9])
+		#end
 	end
 	
 	def depart
