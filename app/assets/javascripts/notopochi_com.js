bfe9bdc8071fd99b3c0f78689ae6321d1b7bf51f
Hayato <Hayato_Matsumuro@amber.plala.or.jp>
2016-01-13 23:07:40 +0900

    fixed #12 着いたボタンをおしたとき、時間と位置情報を送信する。
     ・時間処理を変更
     ・ボタンを押して、位置情報を取得できてから送信

--- app/assets/javascripts/notopochi_com.js
+++ app/assets/javascripts/notopochi_com.js
@@ -1,17 +1,5 @@
 $(document).ready( function(){
 
-	var sleepExitFlg = 0;
-
-	// 着いたボタン押したとき
-	$( "#btn_getoff" ).click(function(){
-		var nowTime = $.now();
-		$('<input>').attr({
-			type: 'hidden',
-			name: 'time' ,
-			value: nowTime 
-		}).appendTo('#formtravelinggetoff' );
-	});
-
 	// 目的地についたボタンを押したとき
 	$( "#btn_arrive" ).click(function(){
 		var nowTime = $.now();
@@ -22,11 +10,18 @@ $(document).ready( function(){
 		}).appendTo('#formtravelingarrive' );
 	});
 
+	// 着いたボタンを押したとき
 	$( "#btn_getoff" ).click(function(){
 		navigator.geolocation.getCurrentPosition(
 			function(position){
 
-				$('#formtravelinggetoff').submit();
+				// 時間を設定する
+				$( '#hidden_getoff_time' ).val( $.now() );
+
+				// 緯度、経度を設定する
+				$( '#hidden_getoff_latitude' ).val( position.coords.latitude );
+				$( '#hidden_getoff_longitude' ).val( position.coords.longitude );
+				$( '#formtravelinggetoff' ).submit();
 			}
 		);
 	});
